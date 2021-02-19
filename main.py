9fdb5701f785a12bc91da7f49ed794b2d730b985
Igor the roofCoder <53712180+russian-droid@users.noreply.github.com>
2021-02-18 22:44:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 '''
+CodeWars kata codewars.com
+
 Write a function called repeat_str which repeats the given string src exactly count times.
 repeatStr(6, "I") // "IIIIII"
 repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
