02e515c962bbc27e703d4e29e767a20b1f31a2a1
Igor the roofCoder <53712180+russian-droid@users.noreply.github.com>
2021-02-18 22:35:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,15 @@ def repeat_str(repeat, string):
     for j in range(repeat):
         new_str +=string
     return new_str    
+
+#------another more efficient way
+def repeat_str(repeat, string):
+    new_str=''
+    new_str=repeat*string
+    return new_str 
+'''
+BECAUSE
+2*'String' -> StringString
+4*('string',) -> ('string', 'string', 'string', 'string')
+3*('good', 'morning') -> ('good', 'morning', 'good', 'morning', 'good', 'morning)
+'''
